df444d13b5a35c47a741fabef47a4591a14a8854
Welito01 <163025971+Welito01@users.noreply.github.com>
2025-09-29 15:11:18 -0300

    Add files via upload
    29/09/2025

--- main.js
+++ main.js
@@ -114,5 +114,58 @@ function comecaCronometro() {
 
 comecaCronometro();
 
-//comecaCronometro();
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
 
+if (tempoFinal > 0) {
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+} else {
+  return [0, 0, 0, 0];
+}
+
+function atualizaCronometro() {
+  document.getElementById("dias0").textContent = calculaTempo(tempos[0]);
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function atualizaCronometro() {
+  document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+  document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+  document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+  document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  if (tempoFinal > 0) {
+    return [dias, horas, minutos, segundos];
+  } else {
+    return [0, 0, 0, 0];
+  }
+}
